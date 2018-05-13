a66ed92ddad743db2976c436a7b3cab826669284
Alois Klink <alois.klink@gmail.com>
2018-05-13 08:42:14 +0100

    Combines contracts and updates them.
    Game automatically restarts after Player 2 makes a move.
    
    Event is printed showing the moves the two players made and the result.
    
    Bets are calculated differently -> proportional to the amount you put
    in.
    
    Refund is given if no one wins the bet.
    
    Fee is kept in bets, and that can be withdrawn by the contract owner.

--- game.sol
+++ game.sol
@@ -4,52 +4,50 @@ pragma solidity ^0.4.0;
 // 1. Deploy and go to 'Run' tab on the right hand side.
 // 2. To play as first player, select any account address.
 // 3. Enter an action number into the 'play' field, 0 for rock, 1 for paper, 3 for scissors.
+//     - Optional: Bet on yourself winning by adding value to the play function (must be over 100 microgwei)
 // 4. Now if you're still using the same account and click 'play' again you'll get an error 
-//     because you've already played.  If you click on 'Result' you'll get error as well 
-//     because only 1 player has played, need a second player to show result.
+//     because you've already played.
 // 5. Now switch to another account as player 2.
-// 6. To play as 2nd player, enter an action number into the 'play' field. Again if you 
-//     press 'play' again you'll get error because you've already played.
-// 7. After the 2nd player has played, you can now click 'Result'.
+// 6. To play as 2nd player, enter an action number into the 'play' field.
+//     - Optional: Bet on yourself winning by adding value to the play function (must be over 100 microgwei)
+// 7. To bet, use the bet() function, give a value of 100microgwei as the bet, and enter:
+//       1 = Player1 win, 2 = Player2 win, 3 = draw
 // 8. Click on the latest console message to show the log, you'll see both players' actions revealed.
 // 9. At the bottom part for showing winner, "winner": 1 if player 1 wins, 2 if player 2 wins, 3 is no winner
+//10. All bets are split up proportional to all the winning bets, with a cut being taken by the contract.
+//11. If there are no winners, all bets are refunded (minus a fee).
+//12. Use withdraw() as the owner to take your winnings.
 
 contract JanKenPonBet {
 	address public owner; // owner of contract, can use to implement special rights
 
-    bool firstPlayer; // has a player played, if no store next action as actionOne, else store as actionTwo
-    uint private actionOne; // store player 1 action
-    uint private actionTwo; // store player 2 action
-    uint winner; // 0 for no winner, 1 for player 1 wins, 2 for player 2 wins
-    mapping(address => Player) public playerInfo; // mapping to store player info
-    Action[] public actions; // dynamically sized array for available actions, will be initialised on construction
-    bool public gameEnd; // keep track if game ended
+    uint private player1move;
     
-    event revealActions(string name, string action); // reveal player action
-    event revealWinner(string name, uint winner); // show winner
+    address public player1Add;
+    string[] public actions = ["Rock", "Paper", "Scissors"];
+    string[] public winner_string = ["Null", "Player1", "Player2", "Draw"];
+    
+    uint constant feePercentage = 98; // 98%, so 2% betting fee
+    
+    event revealActions(string player1action, string player2action, string winner);// reveal game results
     
     struct Action { // struct for storing available action to play, rock/paper/scissors
         string name;
-    }
-    
-    // struct for each player, to store player info
-    struct Player {
-        bool played; // already played an action or not
     }
 
 	// The constructor
 	constructor() public {
 		// msg.sender is a value defined by the user when contract is executed, in this case the address
 		owner = msg.sender;
+		
+		// invalid move to show that we are waiting for player 1
+		player1move = 255;
 
-        // pass actions' names, can be used to add more actions
-        actions.push(Action("rock")); // 0
-        actions.push(Action("paper")); // 1
-        actions.push(Action("scissors")); // 2
+        betters.length = 0;
 	}
 
     // compare actions for deciding winner
-    function game(uint one, uint two) internal returns(uint) { // pass in actions from both player
+    function game(uint one, uint two) pure internal returns(uint) { // pass in actions from both player
         if (one == two) { // tie
             return 3;
         } 
@@ -74,28 +72,147 @@ contract JanKenPonBet {
                 return 2; // rock win
             }
         }
+        return 3;
     }
 
     // for selecting actions
-    function play(uint actionIndex) public {
-        require(!playerInfo[msg.sender].played); // check player has not played before
-        if (!firstPlayer) { // if no first player
-            actionOne = actionIndex; // store as player1's action
-            firstPlayer = true; // first player has played
+    function play(uint actionIndex) public payable {
+        require(actionIndex < actions.length); // check if move is valid
+        if (player1move == 255) { // if no first player
+            player1move = actionIndex; // store as player1's action
+            player1Add = msg.sender;  // stores Player1 Address
+            if (msg.value != 0) {
+                bet(1);
+            }
         } else { // first player already played
-            actionTwo = actionIndex; // store as player2's action
-            gameEnd = true; // 2 players have played, game ends
-            // select winner
-            winner = game(actionOne, actionTwo);
-        }
-        playerInfo[msg.sender].played = true; // set to true when player has selected action
+            // check if player1 != player2
+            require(msg.sender != player1Add); // could remove to save gas
+            
+            if (msg.value != 0) {
+                bet(2);
+            }
+            
+            uint winner = game(player1move, actionIndex); // find winner
+            distributePrizes(winner);
+            Result(winner, actionIndex);
+            
+            player1move = 255;
+        } 
     }
     
     // for revealing game result
-    function Result() public {
-        require(gameEnd); // gameEnd needs to be true to display result
-        revealActions("Player 1", actions[actionOne].name); // display player 1 action
-        revealActions("Player 2", actions[actionTwo].name); // display player 2 action
-        revealWinner("Winner", winner); // display winner
+    function Result(uint winner, uint player2move) internal {
+        emit revealActions(actions[player1move],
+            actions[player2move],
+            winner_string[winner]); // display game actions
+        return;
     }
+    
+    // ----------------------------------- BEGIN BETTING -----------------------
+// Design adapted from: https://medium.com/@merunasgrincalaitis/the-ultimate-end-to-end-tutorial-to-create-and-deploy-a-fully-descentralized-dapp-in-ethereum-18f0cf6d7e0e
+// Every solidity contract must start with the compiler version
+// The one above is the default on the example script in Remix Ethereum
+
+    // 100 microether/szabo is currently like 7 cents
+	uint256 public constant minimumBetValue = 100 szabo;
+	uint256 public totalBetValue;
+	uint256 public constant maxAmountOfBets = 100;
+	address[] public betters; // Array to store the betters' addresses
+
+	// A struct will be made for each person betting on the match
+	struct Better {
+		uint256 betAmount;
+		uint256 playerBettedOn; // There are only two players
+	}
+
+	// Needed so we can use a better's address to figure out his bet and how much he betted
+	mapping(address => Better) public betterInfo;
+
+	// An anonymous callback function that gets executed when ether is sent without executing any function
+	// This saves it and makes sure it isn't lost otherwise the transfer would be rejected
+	function() public payable {}
+	
+	modifier onlyOwner() {
+        require (msg.sender == owner);
+        _;
+    }
+
+	// The kill function - don't use this unless the contract is compromised in some way!
+	function kill() public onlyOwner {
+		selfdestruct(owner);
+	}
+	
+	function withdraw() public onlyOwner {
+		owner.transfer(address(this).balance);
+	}
+
+	// We need to check that the user hasn't placed a bet yet. This checks if they have
+	function checkBetterBetted(address better) public constant returns(bool) {
+		for (uint256 i = 0; i < betters.length; i++) {
+			if (betters[i] == better) return true;
+		}
+		return false;
+	}
+
+	// This allows for a bet of either 1, 2 (i.e. representing the players) or 3 (i.e. a draw)
+	// The "payable" modifier means this function can receive ether when executed
+	function bet(uint256 playerBettedOn) public payable {
+		// "require" is basically an if statement that must return true. If false ether paid is reverted to the sender
+		require(!checkBetterBetted(msg.sender));
+		require(playerBettedOn >= 1 && playerBettedOn <= 3);
+		require(msg.value >= minimumBetValue); // msg.value is the user's ether amount
+
+		betterInfo[msg.sender].betAmount = msg.value;
+		betterInfo[msg.sender].playerBettedOn = playerBettedOn;
+		betters.push(msg.sender);
+		totalBetValue += msg.value;
+	}
+
+	// This function distributes prizes to winning bets
+	function distributePrizes(uint256 winningPlayerNum) internal {
+		// We have to create a temporary in memory array with fixed size (all betters could win)
+		// This gets deleted after the function executes effectively and must be fixed size
+		if(totalBetValue == 0 || betters.length == 0) {
+		    return;
+		}
+		address[] memory winners = new address[](betters.length);
+		uint256 numberOfWinningShares = 0;
+		uint numberOfWinners = 0; 
+
+		for (uint256 i = 0; i < betters.length; i++) {
+			address betterAddress = betters[i];
+
+			if (betterInfo[betterAddress].playerBettedOn == winningPlayerNum) {
+				winners[numberOfWinners] = betterAddress;
+				numberOfWinningShares += betterInfo[betterAddress].betAmount;
+				numberOfWinners++;
+			}
+		}
+
+        if(numberOfWinners == 0) {
+            // no one wins, return all the money (minus a fee of course)
+            for (i = 0; i < betters.length; i++) {
+			    betterAddress = betters[i];
+			    betterAddress.transfer(betterInfo[betterAddress].betAmount
+			        * feePercentage / 100) ;
+            }
+            return;
+        }
+        
+        // Clear all the betters' addresses - the game is done for now
+		betters.length = 0;
+
+        address winner;
+        
+		for (uint256 j = 0; j < numberOfWinners; j++) {
+			// Check that there is in fact a winner for this round then send out the winnings
+			winner = winners[j];
+			if (winner != address(0)) winner.transfer(totalBetValue
+			    * betterInfo[winner].betAmount / numberOfWinningShares
+			    * feePercentage / 100);
+			// no actual need to delete these, but we get a gas refund!
+			delete betterInfo[winner]; 
+		}
+		totalBetValue = 0;
+	}
 }
